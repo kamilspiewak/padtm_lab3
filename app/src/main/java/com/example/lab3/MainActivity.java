740d78ad24e30bb05c67514e1bb06aeec8dbfbf2
Kamil <kamilspiewak97@gmail.com>
2020-05-19 14:43:51 +0200

    partly done

---
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.lab3;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+
+import com.example.lab3.ui.login.LoginActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Kliknięto mnie!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch(id){
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(),"Kliknięto settings!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_two:
+                Toast.makeText(getApplicationContext(),"Kliknięto drugą opcję!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_three:
+                Toast.makeText(getApplicationContext(),"Kliknięto trzecią opcję!",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void kliknij(View obj){
+        Toast.makeText(getApplicationContext(),"Kliknięto przycisk button!",Toast.LENGTH_SHORT).show();
+     //   Intent intencja = new Intent(this, LoginActivity.class);
+     //   startActivity(intencja);
+    }
+
+    public void aparat(View obj){
+       Intent intencja = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+       startActivityForResult(intencja,REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap = (Bitmap) extras.get("data");
+        ConstraintLayout lay = (ConstraintLayout) findViewById(R.id.cont);
+        lay.setBackground(new BitmapDrawable(getResources(),imageBitmap));
+    }
+
+}
